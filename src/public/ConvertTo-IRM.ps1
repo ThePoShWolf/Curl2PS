03debf589c1fd443d9942b4f7afea599ebc7d545
Anthony Howell <anthony@howell-it.com>
2019-03-07 15:03:13 -0800

    properly removes url and for simple examples, outputs a working command

--- src/public/ConvertTo-IRM.ps1
+++ src/public/ConvertTo-IRM.ps1
@@ -5,17 +5,21 @@ Function ConvertTo-IRM {
     )
     # variables
     $headers = @{}
+    $outString = "Invoke-RestMethod"
 
     # Match the url
     # regex from: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
-    $CurlString -match 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+    # I added a comma in the case of multiple parameter values in uri.
+    $CurlString -match 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=,]*)'
     $url = $Matches[0]
 
-    $workingStr = $CurlString -replace '^curl\s+','' -replace $url,''
+    $escapedUrl = [regex]::Escape($url)
+    $workingStr = $CurlString -replace '^curl\s+','' -replace "['`"]?$escapedUrl['`"]?",''
 
     If($url -match 'https?:\/\/(?<up>[^@]+)@'){
         $encodedAuth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Matches.up))
         $headers['Authorization'] = "Basic $encodedAuth"
+        $url = $url -replace "$($Matches.up)@",''
     }
     # Match the parameter name
     While ($workingStr -match '^\-{1,2}(?<param>[^\s]+)'){
@@ -32,44 +36,31 @@ Function ConvertTo-IRM {
 
         $parameterValue = $workingStr.Substring(0,$index)
         $parameterValue = $parameterValue.Substring($parameterLength).Trim()
-
+        #$parameterValue = $parameterValue -replace "^['`"]",''
+        #$parameterValue = $parameterValue -replace "['`"]$",''
         switch ($parameterName){
             {'H','header' -contains $_} {
                 $parameterValue = $parameterValue -replace "^['`"]",''
                 $parameterValue = $parameterValue -replace "['`"]$",''
                 $split = $parameterValue.Split(':')
-                $headers[$split[0]] = $split[1]
+                $headers[($split[0].Trim())] = ($split[1].Trim())
             }
             {'X','request' -contains $_} {
-                "Method"
+                $outString += " -Method $($parameterValue.Trim())"
             }
             {'v','verbose' -contains $_} {
-                "Verbose"
+                $outString += " -Verbose"
             }
-            {'d','data'} {
-                $parameterValue = $parameterValue -replace "^['`"]",''
-                $parameterValue = $parameterValue -replace "['`"]$",''
-                $body = $parameterValue
+            {'d','data' -contains $_} {
+                $outString += " -Body $($parameterValue.Trim())"
             }
             default {
-                "unknown: $($parameterName)"
+                "unknown: $($parameterValue)"
             }
         }
 
         $workingStr = $workingStr.Substring($index).Trim()
     }
+    $outString += " -Headers ('$($headers | ConvertTo-Json -Compress)' | ConvertFrom-Json)"
+    $outString
 }
-
-<#
-#curl -H "X-Auth-Key: 61e5f04ca1794253ed17e6bb986c1702" -H "X-Auth-Workspace: demo.example@actualreports.com" -H "X-Auth-Signature: " -H "Content-Type: application/json" -H "Accept: application/json" -X GET https://us1.pdfgeneratorapi.com/api/v3/templates
-
-$CurlString = @"
-curl -v -X PATCH https://api.airtable.com/v0/appBLvHFF78kERCvW/Payees/recMvdJuoL6ivDA9I -H "Authorization: Bearer YOUR_API_KEY" -H "Content-Type: application/json" -d '{"fields": {"Name": "Eugene Water and Electric Board"}}'
-"@
-
-$CurlString = 'curl --request GET "https://ncg1in-8d1rag:5nuauzj5pkfftlz3fmyksmyhat6j35kf@api.sherpadesk.com/tickets?status=open,onhold&role=user&limit=6&format=json" \  --data ""'
-
-$url = "https://ncg1in-8d1rag:5nuauzj5pkfftlz3fmyksmyhat6j35kf@api.sherpadesk.com/tickets?status=open,onhold&role=user&limit=6&format=json"
-$url -match 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
-
-#>
