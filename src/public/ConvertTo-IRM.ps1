519e1cc2a3df7b771e4e54f04d7497687ee911ab
Anthony Howell <anthony@howell-it.com>
2019-03-22 11:51:57 -0700

    Added some comments

--- src/public/ConvertTo-IRM.ps1
+++ src/public/ConvertTo-IRM.ps1
@@ -13,57 +13,61 @@ Function ConvertTo-IRM {
     $CurlString -match 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=,]*)' | Out-Null
     $url = $Matches[0]
 
+    # Removes curl from the start
+    # Removes the url
     $escapedUrl = [regex]::Escape($url)
     $workingStr = $CurlString -replace '^curl\s+','' -replace "['`"]?$escapedUrl['`"]?",''
 
+    # Match basic auth
     If($url -match 'https?:\/\/(?<up>[^@]+)@'){
         $encodedAuth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Matches.up))
         $headers['Authorization'] = "Basic $encodedAuth"
         $url = $url -replace "$($Matches.up)@",''
     }
 
+    # Add the uri to the output
     $outString += " -Uri '$url'"
 
     # Match the parameter name
     While ($workingStr -match '^(?<param>\-{1,2}[^\s]+)'){
         $parameterName = $Matches.param
-        #$parameterLength = $Matches[0].Length
         
         # Match parameter value
         # Don't match quotes except for excaped quotes: \"
         $escapedParamName = [regex]::Escape($parameterName)
         $workingStr -match "$escapedParamName (?<paramValueQuotes>`'(?<paramValue>[^']+)`'|`"(?<paramValue>((\\`")|[^`"])+)`"|(?<paramValue>[^\s]+))" | Out-Null
 
-        #$parameterValue = $workingStr.Substring(0,$index)
-        #$parameterValue = $parameterValue.Substring($parameterLength).Trim()
-        #$parameterValue = $parameterValue -replace "^['`"]",''
-        #$parameterValue = $parameterValue -replace "['`"]$",''
+        # Do things based on what parameter it is
         switch ($parameterName.Trim('-')){
             {'H','header' -contains $_} {
-                #$parameterValue = $parameterValue -replace "^['`"]",''
-                #$parameterValue = $parameterValue -replace "['`"]$",''
-                #$split = $parameterValue.Split(':')
+                # Headers
                 $split = ($Matches.paramValue.Split(':') -replace '\\"','"')
                 $headers[($split[0].Trim())] = (($split[1..$($split.count)] -join ':').Trim())
             }
             {'X','request' -contains $_} {
+                # Request type
                 $outString += " -Method $($matches.paramValueQuotes.Trim())"
             }
             {'v','verbose' -contains $_} {
+                # Verbosity
                 $outString += " -Verbose"
             }
             {'d','data' -contains $_} {
+                # Body
                 $outString += (" -Body '$($matches.paramValue.Trim())'" -replace '\\"','"')
             }
             default {
+                # Unknown
                 "unknown: $($matches[0])"
             }
         }
 
-        #$workingStr = $workingStr.Substring($index).Trim()
+        # Remove the param name and value from the workingStr
         $escapedMatch = [regex]::Escape($Matches[0])
         $workingStr = ($workingStr -replace $escapedMatch,'').Trim()
     }
+    # Add headers on the end
+    # To output as string, converting them to json and adding ConvertFrom-Json
     $outString += " -Headers ('$($headers | ConvertTo-Json -Compress)' | ConvertFrom-Json)"
     $outString
 }
