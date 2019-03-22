15b227be7c3be8e4079fb94a9aff5857ca582bd7
Anthony Howell <anthony@howell-it.com>
2019-03-22 11:05:00 -0700

    #2 now supports strings with quotes for values

--- src/public/ConvertTo-IRM.ps1
+++ src/public/ConvertTo-IRM.ps1
@@ -10,7 +10,7 @@ Function ConvertTo-IRM {
     # Match the url
     # regex from: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
     # I added a comma in the case of multiple parameter values in uri.
-    $CurlString -match 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=,]*)'
+    $CurlString -match 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=,]*)' | Out-Null
     $url = $Matches[0]
 
     $escapedUrl = [regex]::Escape($url)
@@ -22,47 +22,47 @@ Function ConvertTo-IRM {
         $url = $url -replace "$($Matches.up)@",''
     }
 
-    $outString += " -Uri $url"
+    $outString += " -Uri '$url'"
 
     # Match the parameter name
-    While ($workingStr -match '^\-{1,2}(?<param>[^\s]+)'){
+    While ($workingStr -match '^(?<param>\-{1,2}[^\s]+)'){
         $parameterName = $Matches.param
-        $parameterLength = $Matches[0].Length
+        #$parameterLength = $Matches[0].Length
         
-
         # Match parameter value
-        if ($workingStr -match '\s\-\S'){
-            $index = $workingStr.IndexOf($Matches[0])
-        } else {
-            $index = $workingStr.Length
-        }
+        # Don't match quotes
+        $escapedParamName = [regex]::Escape($parameterName)
+        $workingStr -match "$escapedParamName (?<paramValueQuotes>`'(?<paramValue>[^']+)`'|`"(?<paramValue>[^`"]+)`"|(?<paramValue>[^\s]+))" | Out-Null
 
-        $parameterValue = $workingStr.Substring(0,$index)
-        $parameterValue = $parameterValue.Substring($parameterLength).Trim()
+        #$parameterValue = $workingStr.Substring(0,$index)
+        #$parameterValue = $parameterValue.Substring($parameterLength).Trim()
         #$parameterValue = $parameterValue -replace "^['`"]",''
         #$parameterValue = $parameterValue -replace "['`"]$",''
-        switch ($parameterName){
+        switch ($parameterName.Trim('-')){
             {'H','header' -contains $_} {
-                $parameterValue = $parameterValue -replace "^['`"]",''
-                $parameterValue = $parameterValue -replace "['`"]$",''
-                $split = $parameterValue.Split(':')
+                #$parameterValue = $parameterValue -replace "^['`"]",''
+                #$parameterValue = $parameterValue -replace "['`"]$",''
+                #$split = $parameterValue.Split(':')
+                $split = $Matches.paramValue.Split(':')
                 $headers[($split[0].Trim())] = ($split[1].Trim())
             }
             {'X','request' -contains $_} {
-                $outString += " -Method $($parameterValue.Trim())"
+                $outString += " -Method $($matches.paramValueQuotes.Trim())"
             }
             {'v','verbose' -contains $_} {
                 $outString += " -Verbose"
             }
             {'d','data' -contains $_} {
-                $outString += " -Body $($parameterValue.Trim())"
+                $outString += " -Body $($matches.paramValueQuotes.Trim())"
             }
             default {
-                "unknown: $($parameterValue)"
+                "unknown: $($matches[0])"
             }
         }
 
-        $workingStr = $workingStr.Substring($index).Trim()
+        #$workingStr = $workingStr.Substring($index).Trim()
+        $escapedMatch = [regex]::Escape($Matches[0])
+        $workingStr = ($workingStr -replace $escapedMatch,'').Trim()
     }
     $outString += " -Headers ('$($headers | ConvertTo-Json -Compress)' | ConvertFrom-Json)"
     $outString
