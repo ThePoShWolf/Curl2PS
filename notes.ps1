f58e5f48076c76a300fb33e32f54732dc27b96ff
Anthony Howell <anthony@howell-it.com>
2019-10-11 10:17:47 -0700

    added notes for livestream

---
+++ notes.ps1
@@ -0,0 +1,4 @@
+[Microsoft.CodeAnalysis.CommandLineParser]::SplitCommandLineIntoArguments('curl -H "X-Auth-Key: 61e5f04ca1794253ed17e6bb986c1702" -H "X-Auth-Workspace: demo.example@actualreports.com" -H "X-Auth-Signature: " -H "Content-Type: application/json" -H "Accept: application/json" -X GET https://us1.pdfgeneratorapi.com/api/v3/templates',$true)
+[Microsoft.CodeAnalysis.CommandLineParser]::SplitCommandLineIntoArguments
+
+[System.Uri]::new('https://ncg1in-8d1rag:5nuauzj5pkfftlz3fmyksmyhat6j35kf@api.sherpadesk.com/tickets?status=open,onhold&role=user&limit=6&format=json')
