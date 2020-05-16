ee675a7848de33294d2b20cf057fb378e4f8057c
Anthony Howell <anthony@howell-it.com>
2020-05-16 12:33:36 -0700

    and removed the dll from using

--- src/using.ps1
+++ src/using.ps1
@@ -3,7 +3,6 @@ if ($PSVersionTable.PSVersion.Major -lt 6) {
         'System.Collections.Immutable.dll'
         'System.Reflection.Metadata.dll'
         'Microsoft.CodeAnalysis.dll'
-        'Microsoft.CodeAnalysis.CSharp.dll'
     )
     foreach ($dll in $dlls2Load){
         Add-Type -Path "$PSScriptRoot\dependencies\$dll"
