9f81b69d7b99401c4be2ac0a81085adc965e4d36
Anthony Howell <anthony@howell-it.com>
2022-06-28 04:54:43 -0700

    only import module if not already imported

--- build.ps1
+++ build.ps1
@@ -3,12 +3,14 @@ param(
     [string[]]$Task = 'ModuleBuild'
 )
 
-$DependentModules = @('PSDeploy','InvokeBuild','PlatyPS','Pester') # add pester when pester tests are added
-Foreach ($Module in $DependentModules){
-    If (-not (Get-Module $module -ListAvailable)){
-        Install-Module -name $Module -Scope CurrentUser -Force
+$DependentModules = @('PSDeploy', 'InvokeBuild', 'PlatyPS', 'Pester')
+foreach ($Module in $DependentModules) {
+    if (-not (Get-Module $module -ListAvailable)) {
+        Install-Module -Name $Module -Scope CurrentUser -Force
     }
-    Import-Module $module -ErrorAction Stop
+    if (-not (Get-Module $module)) {
+        Import-Module $module -ErrorAction Stop
+    }
 }
 # Builds the module by invoking InvokeBuild
 Invoke-Build "$PSScriptRoot\Curl2PS.build.ps1" -Task $Task
