a382d5edaef660c5338103b1fbb30b5d9b01d699
Anthony Howell <anthony@howell-it.com>
2025-04-24 09:00:24 -0700

    functionizing the splat generation

--- src/Invoke-Curl2PS.ps1
+++ src/Invoke-Curl2PS.ps1
@@ -55,25 +55,7 @@ Function Invoke-Curl2PS {
     }
 
     # generate a splat representation of the parameters
-    $splat = @{}
-    foreach ($paramGroup in $parameters | Group-Object ParameterName) {
-        if ($paramGroup.Count -gt 1) {
-            if ($paramGroup[0].Group[0].Type -eq 'Hashtable') {
-                $ht = @{}
-                foreach ($pg in $paramGroup.Group) {
-                    foreach ($key in $pg.Value.Keys) {
-                        $ht[$key] = $pg.Value[$key]
-                    }
-                }
-                $splat[$paramGroup.Name] = $ht
-            } else {
-                Write-Warning "Multiple values for parameter $($paramGroup.Name) of type $($paramGroup.Group[0].TypeNames) are not supported."
-            }
-        } else {
-            $splat[$paramGroup.Name] = $paramGroup.Group[0].Value
-        }
-    }
-    $splat
+    Get-Curl2PSSplat -Parameters $parameters
 
     # generate a string representation of the Invoke-RestMethod command
     Write-Curl2PSStringOutput -Parameters $parameters
@@ -185,4 +167,30 @@ Function Write-Curl2PSStringOutput {
         }
     }
     $baseStr
+}
+
+Function Get-Curl2PSSplat {
+    [OutputType([hashtable])]
+    param (
+        [Curl2PSParameterDefinition[]]$Parameters
+    )
+    $splat = @{}
+    foreach ($paramGroup in $parameters | Group-Object ParameterName) {
+        if ($paramGroup.Count -gt 1) {
+            if ($paramGroup[0].Group[0].Type -eq 'Hashtable') {
+                $ht = @{}
+                foreach ($pg in $paramGroup.Group) {
+                    foreach ($key in $pg.Value.Keys) {
+                        $ht[$key] = $pg.Value[$key]
+                    }
+                }
+                $splat[$paramGroup.Name] = $ht
+            } else {
+                Write-Warning "Multiple values for parameter $($paramGroup.Name) of type $($paramGroup.Group[0].TypeNames) are not supported."
+            }
+        } else {
+            $splat[$paramGroup.Name] = $paramGroup.Group[0].Value
+        }
+    }
+    $splat
 }
