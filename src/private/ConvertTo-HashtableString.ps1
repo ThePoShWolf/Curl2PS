d07938b4c29557a0e7124c9d76c120bee5f3f2a4
Anthony Howell <anthony@howell-it.com>
2025-04-24 20:25:33 -0700

    supporting nested hashtables

--- src/private/ConvertTo-HashtableString.ps1
+++ src/private/ConvertTo-HashtableString.ps1
@@ -1,11 +1,21 @@
 Function ConvertTo-HashtableString {
     param (
-        [Hashtable]$InputObject
+        [Hashtable]$InputObject,
+        [int]$Depth = 0
     )
     $strKeys = @()
+    $indent = "    " * $Depth  # Indentation based on depth
     foreach ($key in $InputObject.Keys | Sort-Object) {
-        $strKeys += "    '$key' = '$($InputObject[$key])'"
+        $value = $InputObject[$key]
+        if ($value -is [Hashtable]) {
+            # recursively process nested hashtable
+            $nestedHashtableString = ConvertTo-HashtableString -InputObject $value -Depth ($Depth + 1)
+            $strKeys += "$indent    '$key' = $nestedHashtableString"
+        } else {
+            # depth based nesting
+            $strKeys += "$indent    '$key' = '$value'"
+        }
     }
-    $str = "@{`n" + ($strKeys -join "`n") + "`n}"
+    $str = "$indent@{`n" + ($strKeys -join "`n") + "`n$indent}"
     $str
 }
