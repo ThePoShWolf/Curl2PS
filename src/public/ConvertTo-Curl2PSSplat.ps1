5eca65fe957e52534fb256d566fc4a7f8521bf5d
Anthony Howell <anthony@howell-it.com>
2025-04-28 07:50:14 -0700

    improved pipeline handling

--- src/public/ConvertTo-Curl2PSSplat.ps1
+++ src/public/ConvertTo-Curl2PSSplat.ps1
@@ -5,30 +5,33 @@ Function ConvertTo-Curl2PSSplat {
             Mandatory,
             ValueFromPipeline
         )]
-        [Curl2PSParameterDefinition[]]$Parameters
+        [Curl2PSParameterDefinition]$Parameter
     )
-    $splat = @{}
-    foreach ($paramGroup in $parameters | Group-Object ParameterName) {
-        if ($paramGroup.Count -gt 1) {
-            if ($paramGroup[0].Group[0].Type -eq 'Hashtable') {
-                $ht = @{}
-                foreach ($pg in $paramGroup.Group) {
-                    foreach ($key in $pg.Value.Keys) {
-                        $ht[$key] = $pg.Value[$key]
-                    }
+    Begin {
+        $splat = @{}
+    }
+    Process {
+        if ($Parameter.Type -eq 'Hashtable') {
+            $ht = @{}
+            if ($splat.Keys -contains $Parameter.ParameterName) {
+                foreach ($key in $splat[$Parameter.ParameterName].Keys) {
+                    $ht[$key] = $splat[$Parameter.ParameterName]
                 }
-                try {
-                    $convertedHt = Invoke-GetItemInHashtable $ht
-                } catch {
-                    $convertedHt = $ht
-                }
-                $splat[$paramGroup.Name] = $convertedHt
-            } else {
-                Write-Warning "Multiple values for parameter $($paramGroup.Name) of type '$($paramGroup.Group[0].Type)' are not supported."
             }
+            foreach ($key in $Parameter.Value.Keys) {
+                $ht[$key] = $Parameter.Value[$key]
+            }
+            try {
+                $convertedHt = Invoke-GetItemInHashtable $ht
+            } catch {
+                $convertedHt = $ht
+            }
+            $splat[$ParameterName] = $convertedHt
         } else {
-            $splat[$paramGroup.Name] = $paramGroup.Group[0].Value
+            $splat[$Parameter.ParameterName] = $Parameter.Value
         }
     }
-    $splat
+    End {
+        $splat
+    }
 }
