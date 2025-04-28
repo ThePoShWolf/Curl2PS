5927468710984366333f8311c17be63d2001b52a
Anthony Howell <anthony@howell-it.com>
2025-04-28 08:44:42 -0700

    removing unneeded file

--- Module.PSDeploy.ps1
+++
@@ -1,9 +0,0 @@
-Deploy Module {
-    By PSGalleryModule {
-        FromSource Build\Curl2PS
-        To PSGallery
-        WithOptions @{
-            ApiKey = $ENV:PSGalleryKey
-        }
-    }
-}
